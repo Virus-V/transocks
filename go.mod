24fd82e8e0856b215b65795635f8195a7688efc3
Virus.V <virusv@live.com>
2023-10-18 17:20:23 +0800

    add proxy dialer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module transocks_ng
 
 go 1.21.2
+
+require golang.org/x/net v0.17.0
--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"net/url"
 	"sync"
+	"time"
+
+	"golang.org/x/net/proxy"
 )
 
 var (
@@ -17,15 +21,18 @@ var (
 var wg sync.WaitGroup
 
 func main() {
-	fmt.Println("hello world")
+	flag.Parse()
 
-	fmt.Printf("listen %s\n", *listenAddr)
+	fmt.Printf("Listen %s\n", *listenAddr)
+	fmt.Printf("Proxy server:%s\n", *proxyAddr)
+
 	ln, err := net.Listen("tcp", *listenAddr)
 	if err != nil {
 		fmt.Println("listen failed!")
 		return
 	}
 
+	/* set transparent proxy options */
 	lntcp := ln.(*net.TCPListener)
 	sysConn, err := lntcp.SyscallConn()
 	if err != nil {
@@ -39,12 +46,30 @@ func main() {
 		return
 	}
 
+	/* buf pool */
 	bufPool := sync.Pool{
 		New: func() interface{} {
 			return make([]byte, 1024)
 		},
 	}
 
+	/* setup socks proxy dialer */
+	proxyUrl, err := url.Parse(*proxyAddr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	proxyDialer, err := proxy.FromURL(proxyUrl, &net.Dialer{
+		KeepAlive: 3 * time.Minute,
+		DualStack: true,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	/* proxy main loop */
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -52,36 +77,48 @@ func main() {
 			continue
 		}
 
-		fmt.Println("new connect addr ", conn.LocalAddr().String())
-		destConn, err := net.Dial("tcp", conn.LocalAddr().String())
+		fmt.Println("Start proxy:", conn.LocalAddr().String())
+
+		//destConn, err := net.Dial("tcp", conn.LocalAddr().String())
+		destConn, err := proxyDialer.Dial("tcp", conn.LocalAddr().String())
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
+		/* proxy dest write */
 		go func(ctx context.Context) error {
 			var written int64
+
 			wg.Add(1)
+
 			buf := bufPool.Get().([]byte)
 			written, err := io.CopyBuffer(destConn, conn, buf)
 			bufPool.Put(buf)
+
 			conn.Close()
 			wg.Done()
-			fmt.Printf("local to remote: %d\n", written)
+			fmt.Printf("dest write channel closed, written: %d Bytes\n", written)
 			return err
 		}(context.Background())
 
+		/* proxy dest read */
 		go func(ctx context.Context) error {
 			var written int64
+
 			wg.Add(1)
+
 			buf := bufPool.Get().([]byte)
 			written, err := io.CopyBuffer(conn, destConn, buf)
 			bufPool.Put(buf)
 			conn.Close()
+
 			wg.Done()
-			fmt.Printf("remote to local: %d\n", written)
+			fmt.Printf("dest read channel closed, written: %d Bytes\n", written)
 			return err
 		}(context.Background())
 	}
+
+	/* TOOD: 优雅地退出 */
 	wg.Wait()
 }
